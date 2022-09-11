0aaec1490f333395063a1522e1967f75b3de895a
Ryanksa <ryanlin58@gmail.com>
2022-09-10 21:40:28 -0400

    Fixed vercel build issue with edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
