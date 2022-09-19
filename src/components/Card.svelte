97dbd5a1b0efbcd1523017bd695b975ba92e9df7
Ryanksa <ryanlin58@gmail.com>
2022-09-18 20:25:19 -0400

    Added projects to portfolio

---
+++ src/components/Card.svelte
@@ -0,0 +1,57 @@
+<script>
+  export let header;
+  export let body;
+  export let url;
+
+  import { spring } from "svelte/motion";
+  import { fly } from "svelte/transition";
+
+  let fill = spring(0, {
+    stiffness: 0.03,
+    damping: 0.3,
+  });
+  let hovering;
+  let unhovering;
+
+  const handleMouseEnter = url
+    ? () => {
+        clearInterval(unhovering);
+        hovering = setInterval(() => {
+          if ($fill < 100) {
+            fill.set(Math.max($fill + 5, 100));
+          } else {
+            clearInterval(hovering);
+            window.open(url, "_blank");
+          }
+        }, 100);
+      }
+    : null;
+
+  const handleMouseLeave = url
+    ? () => {
+        clearInterval(hovering);
+        unhovering = setInterval(() => {
+          if ($fill > 0) {
+            fill.set(Math.min($fill - 5, 0));
+          } else {
+            clearInterval(unhovering);
+          }
+        }, 100);
+      }
+    : null;
+</script>
+
+<div
+  class="max-w-sm flex flex-col justify-center items-center p-4 bg-slate-300/25 rounded-3xl relative overflow-hidden"
+  transition:fly={{ y: 30, duration: 300 }}
+  on:mouseenter={handleMouseEnter}
+  on:mouseleave={handleMouseLeave}
+>
+  <div
+    class="w-full absolute left-0 bottom-0 rounded-xl bg-slate-400/25"
+    style:height={`${$fill}%`}
+  />
+  <div class="text-2xl font-bold mb-2">{header}</div>
+  <div class="w-4/5 h-[3px] bg-indigo-400 rounded-xl" />
+  <div class="text-lg mt-2 p-2">{body}</div>
+</div>
---
+++ src/components/Link.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let text;
+  export let icon;
+  export let url;
+
+  import { spring } from "svelte/motion";
+
+  let fill = spring(0, {
+    stiffness: 0.03,
+    damping: 0.3,
+  });
+  let hovering;
+  let unhovering;
+
+  const handleMouseEnter = url
+    ? () => {
+        clearInterval(unhovering);
+        hovering = setInterval(() => {
+          if ($fill < 100) {
+            fill.set(Math.max($fill + 5, 100));
+          } else {
+            clearInterval(hovering);
+            window.open(url, "_blank");
+          }
+        }, 100);
+      }
+    : null;
+
+  const handleMouseLeave = url
+    ? () => {
+        clearInterval(hovering);
+        unhovering = setInterval(() => {
+          if ($fill > 0) {
+            fill.set(Math.min($fill - 5, 0));
+          } else {
+            clearInterval(unhovering);
+          }
+        }, 100);
+      }
+    : null;
+</script>
+
+<div
+  class="flex items-center gap-2 w-fit py-1 pl-2 pr-6 rounded-xl relative overflow-hidden"
+  on:mouseenter={handleMouseEnter}
+  on:mouseleave={handleMouseLeave}
+>
+  <div
+    class="h-full absolute left-0 top-0 rounded-xl bg-slate-300/25"
+    style:width={`${$fill}%`}
+  />
+  {#if icon}
+    <img src={icon} alt="" class="w-6 h-6" />
+  {/if}
+  {text}
+</div>
