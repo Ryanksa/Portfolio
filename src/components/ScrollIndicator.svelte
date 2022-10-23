fba6b193048c153ab35adc04c9844f7f11145e18
Ryanksa <ryanlin58@gmail.com>
2022-10-23 16:56:51 -0400

    Added scroll indicator to page

---
+++ src/components/ScrollIndicator.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let hidden = false;
+
+  onMount(() => {
+    const onMouseWheel = (e: WheelEvent) => {
+      if (e.deltaY > 0) hidden = true;
+    };
+    // @ts-ignore
+    window.addEventListener("mousewheel", onMouseWheel);
+    // @ts-ignore
+    () => window.removeEventListener("mousewheel", onMouseWheel);
+  });
+</script>
+
+{#if !hidden}
+  <div
+    class="fixed bottom-1 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center"
+  >
+    <span>scroll down</span>
+    <span class="animate-bounce">v</span>
+  </div>
+{/if}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      keyframes: {
-        expand: {
-          "0%": { transform: "scaleX(0)" },
-          "100%": { transform: "scaleX(1)" },
-        },
-      },
-      animation: {
-        "divider-expand": "expand 300ms ease-in 150ms forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
